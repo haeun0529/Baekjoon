3d4356ca0048fd2ae7874868c6e227364d750a79
Haeun <h991512@naver.com>
2025-09-16 17:06:16 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+
+    char str[101];
+    int result = 0;
+    for(int i=0; i<n; i++) {
+        scanf("%s", str);
+        int len = strlen(str);
+        char prev = 0;
+        int ap[26] = {0};
+        int flag = 1;
+
+        for(int j=0; j<len; j++) {
+            if(str[j] != prev) { // 현재 문자가 이전 문자와 같지 않은 경우
+                if(ap[str[j] - 'a']) { // 안나온 문자인 경우
+                    flag = 0;
+                    break;
+                }
+                ap[str[j] - 'a'] = 1;
+                prev = str[j];
+            }
+        }
+        if(flag) result++;
+    }
+    printf("%d", result);
+
+    return 0;
+}
