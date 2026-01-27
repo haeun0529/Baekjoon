cc35d865e26d074a476a330180024bfd48e5ea05
Haeun <h991512@naver.com>
2026-01-27 17:56:05 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    int r1, r2, s;
+
+    cin >> r1 >> s;
+
+    r2 = 2*s - r1;
+
+    cout << r2;
+
+    return 0;
+}
