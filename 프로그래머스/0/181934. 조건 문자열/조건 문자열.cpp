0b0be6f029673b824fe24a80174ffe822fbe1e62
Haeun <h991512@naver.com>
2026-04-22 20:53:09 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 3.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    
+    if(ineq==">" && eq=="=") {
+        if(n >= m) answer = 1;
+        else answer = 0;
+    }
+    else if(ineq=="<" && eq=="=") {
+        if(n <= m) answer = 1;
+        else answer = 0;
+    }
+    else if(ineq==">" && eq=="!") {
+        if(n > m) answer = 1;
+        else answer = 0;
+    }
+    else {
+        if(n < m) answer = 1;
+        else answer = 0;
+    } 
+    
+    return answer;
+}
