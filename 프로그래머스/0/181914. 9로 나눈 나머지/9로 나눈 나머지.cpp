28165cf937256879e26e4cbdcac9ac9c864abeff
Haeun <h991512@naver.com>
2026-04-25 19:37:01 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.08 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string number) {
+    int answer;
+    
+    int sum = 0;
+    for(int i=0; i<number.size(); i++)
+        sum += number[i] - '0';
+    
+    answer = sum % 9;
+    
+    return answer;
+}
