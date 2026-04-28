586b335c1d14111a8dd408cd22468e10c1ed5beb
Haeun <h991512@naver.com>
2026-04-28 10:24:13 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    int answer = 0;
+    
+    vector<string> temp;
+    for(int i=0; i<my_string.size(); i++) {
+        temp.push_back(my_string.substr(i));
+    }
+    
+    for(int i=0; i<temp.size(); i++) {
+        if(temp[i] == is_suffix) {
+            answer = 1;
+            break;
+        }
+    }
+    
+    return answer;
+}
