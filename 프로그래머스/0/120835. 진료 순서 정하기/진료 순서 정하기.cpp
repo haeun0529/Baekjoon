3f954bf06bf3c460afdfc0a787e5108877d372fe
Haeun <h991512@naver.com>
2026-05-07 12:54:50 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> emergency) {
+    vector<int> answer;
+    
+    for(int i=0; i<emergency.size(); i++) {
+        int count = 1;
+        for(int j=0; j<emergency.size(); j++) {
+            if(emergency[i] < emergency[j])
+                count++;
+        }
+        answer.push_back(count);
+    }
+    
+    return answer;
+}
