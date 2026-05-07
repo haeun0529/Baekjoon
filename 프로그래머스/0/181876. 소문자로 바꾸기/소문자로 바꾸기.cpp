fa576c61c7163be3f60bd97a0102fecfc84c1c16
Haeun <h991512@naver.com>
2026-05-07 12:16:05 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(char& c : myString) c = tolower(c); 
+    return myString;
+}
