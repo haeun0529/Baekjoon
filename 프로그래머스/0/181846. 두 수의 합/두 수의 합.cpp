087673cbbc43a9a746795815df9fa63cc285aac9
Haeun <h991512@naver.com>
2026-05-12 14:11:28 +0900

    [level 0] Title: 두 수의 합, Time: 123.44 ms, Memory: 4.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string a, string b) {
+    string result = "";
+    
+    int carry = 0;
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    
+    while(i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+        if(i >= 0) sum += a[i--] - '0';
+        if(j >= 0) sum += b[j--] - '0';
+        
+        carry = sum / 10;
+        result = to_string(sum % 10) + result;
+    }
+    
+    return result;
+}
