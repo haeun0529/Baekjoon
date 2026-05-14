6962ba96f53b11a960e5d40b0c01413c54e872bc
Haeun <h991512@naver.com>
2026-05-14 13:42:20 +0900

    [level 1] Title: 모의고사, Time: 0.03 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    
+    vector<int> p1 = { 1,2,3,4,5 };
+    vector<int> p2 = { 2,1,2,3,2,4,2,5 };
+    vector<int> p3 = { 3,3,1,1,2,2,4,4,5,5 };
+    
+    int count1=0, count2=0, count3=0;
+    for(int i=0; i<answers.size(); i++) {
+        if(answers[i] == p1[i%p1.size()]) count1++;
+        if(answers[i] == p2[i%p2.size()]) count2++;
+        if(answers[i] == p3[i%p3.size()]) count3++;
+    }
+    
+    vector<int> n;
+    n.push_back(count1);
+    n.push_back(count2);
+    n.push_back(count3);
+    
+    int max = *max_element(n.begin(), n.end());
+    for(int i=0; i<n.size(); i++) {
+        if(n[i] == max)
+            answer.push_back(i+1);
+    }
+    
+    
+    return answer;
+}
