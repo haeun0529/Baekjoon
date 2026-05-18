790069c024d77ebb53d40f097ded3c691d8b9f18
Haeun <h991512@naver.com>
2026-05-18 14:21:12 +0900

    [level 0] Title: 그림 확대, Time: 0.08 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> picture, int k) {
+    vector<string> answer;
+    
+    for(int i=0; i<picture.size(); i++) {
+        string temp = "";
+        for(int j=0; j<picture[i].size(); j++) {
+            for(int r=0; r<k; r++)
+                temp += picture[i][j];
+        }
+        for(int j=0; j<k; j++)
+            answer.push_back(temp);
+    }
+    
+    return answer;
+}
