4e2c17ded94a569db510d4d2c1de407a99395265
Haeun <h991512@naver.com>
2026-05-19 10:46:42 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+    
+    if(s1.size() <= s2.size()) {
+        for(int i=0; i<s1.size(); i++) {
+            if(find(s2.begin(), s2.end(), s1[i]) != s2.end())
+                answer++;
+        }
+    }
+    
+    return answer;
+}
